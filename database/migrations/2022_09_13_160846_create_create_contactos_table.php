6a322ff309450f00a3f8b8a7f0042c6a1b349f1a
LeanBrizu <leandrobri.ar@gmail.com>
2024-01-13 00:40:25 -0300

    [Mejora] Eliminar salto de línea

--- database/migrations/2022_09_13_160846_create_create_contactos_table.php
+++ database/migrations/2022_09_13_160846_create_create_contactos_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
     public function up()
     {
         Schema::create('contactos', function (Blueprint $table) {
-            
             $table->id();
             $table->string('nombre');
             $table->string('apellido');
--- database/migrations/2022_09_13_160904_create_create_noticias_table.php
+++ database/migrations/2022_09_13_160904_create_create_noticias_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
     public function up()
     {
         Schema::create('noticias', function (Blueprint $table) {
-            
             $table->id();
             $table->string('titulo');
             $table->text('copete');
