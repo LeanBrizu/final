c0a4c07de933fc7b3d92a3a90370ae7a830d6550
LeanBrizu <leandrobri.ar@gmail.com>
2024-01-13 01:32:53 -0300

    [Mejora] Eliminar salto de línea.

--- database/migrations/2014_10_12_000000_create_usuarios_table.php
+++ database/migrations/2014_10_12_000000_create_usuarios_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
     public function up()
     {
         Schema::create('usuarios', function (Blueprint $table) {
-            
             $table->id();
             $table->string('name');
             $table->string('password');
